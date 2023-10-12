5a339185112f4c3ff585ef1c2e4666f6bd262a16
kk2 <104248260+kk2a@users.noreply.github.com>
2023-10-12 14:51:08 +0900

    情報を入れれるようにした

--- main/kuso_game.py
+++ main/kuso_game.py
@@ -2,17 +2,41 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120, title="Hello Pyxel")
+        pyxel.init(300, 250, title="kuso game")
+        pyxel.mouse(True)
+        self.info()
+
         pyxel.run(self.update, self.draw)
+
+    def info(self):
+        with open('./choimuzu.txt') as f:
+            line = f.readline()
+
+            # rstripはいらないかもだけど怖いから入れておきます
+            self.yuusha_strength, self.tower_num = map(int, line.rstrip("\n").split())
+            self.tower_info = []
+            for _ in range(self.tower_num):
+                line = f.readline()
+                T = int(line.rstrip("\n"))
+                a = []
+                for __ in range(T):
+                    line = f.readline()
+                    a.append(list(map(int, line.rstrip("\n").split())))
+                self.tower_info.append(a)
+
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+        
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
-        pyxel.blt(61, 66, 0, 0, 0, 38, 16)
+        pyxel.cls(1)
+
+        self.draw_tower()
+    
+    def draw_tower(self):
+        pyxel.rectb(150, 190, 60, 50, 0)
 
 
 App()

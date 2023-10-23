f1feef1f643e6bec218334054e36c8f6a81d6771
kk2 <104248260+kk2a@users.noreply.github.com>
2023-10-23 21:34:43 +0900

    敵の待機アニメーションの追加

--- main/kuso_game.py
+++ main/kuso_game.py
@@ -116,14 +116,35 @@ class App:
     def draw_tower(self):
         slide = self.left_slide
 
-        # タワー、敵の描画
+        u = (
+            ((33, 33, 0, 0, 0, 24, 32, 15), (33, 33, 0, 32, 0, 24, 32, 15)),
+            ((30, 19, 0, 0, 32, 24, 16, 15), (30, 19, 0, 32, 32, 24, 16, 15)),
+            ((35, 14, 0, 0, 59, 12, 10, 15), (35, 14, 0, 16, 59, 12, 10, 15)),
+            ((28, 33, 0, 0, 72, 32, 32, 15), (28, 33, 0, 32, 72, 32, 32, 15)),
+            ((33, 33, 0, 0, 104, 24, 32, 15), (33, 33, 0, 32, 104, 24, 32, 15)),
+            ((28, 33, 0, 0, 144, 32, 32, 15), (28, 33, 0, 32, 144, 32, 32, 15)),
+            ((26, 25, 0, 56, 0, 32, 24, 15), (26, 25, 0, 88, 0, 32, 24, 15))
+        )
+
+        v = (
+            (35, 24, 0, 0, 216, 16, 23, 5),
+            (35, 24, 0, 24, 216, 16, 23, 5),
+            (35, 24, 0, 48, 216, 16, 23, 5)
+        )
+
+        # タワー
         pyxel.rectb(40 - slide, 240, 60, 50, 0)
-        pyxel.load(self.load_path[1])
         for i in range(self.tower_num):
             T = len(self.tower_info[i])
             for j in range(T):
                 pyxel.rectb(40 + 100 * (i + 1) - slide,
                             240 - 50 * j, 60, 50, 0)
+
+        # 敵
+        pyxel.load(self.load_path[1])
+        for i in range(self.tower_num):
+            T = len(self.tower_info[i])
+            for j in range(T):
                 t, b, m, idx = self.tower_info[i][j]
                 if t == 1:
                     pyxel.text(
@@ -132,23 +153,16 @@ class App:
                         f"{self.text[b * 2 - 2]}{m}",
                         8
                     )
-                    u = [
-                        [28, 33, 0, 0, 0, 32, 32, 1],
-                        [28, 20, 0, 8, 32, 32, 16, 15],
-                        [35, 15, 0, 1, 59, 12, 10, 15],
-                        [28, 31, 0, 3, 73, 26, 30, 15],
-                        [28, 33, 0, 8, 104, 24, 32, 15],
-                        [28, 34, 0, 0, 207, 26, 33, 15]
-                    ]
+                    k = (pyxel.frame_count) // 25 % 2  # 一般には % len(u[idx])
                     pyxel.blt(
-                        40 + 100 * (i + 1) - slide + u[idx][0],
-                        290 - 50 * j - u[idx][1],
-                        u[idx][2],
-                        u[idx][3],
-                        u[idx][4],
-                        u[idx][5],
-                        u[idx][6],
-                        u[idx][7]
+                        40 + 100 * (i + 1) - slide + u[idx][k][0],
+                        290 - 50 * j - u[idx][k][1],
+                        u[idx][k][2],
+                        u[idx][k][3],
+                        u[idx][k][4],
+                        u[idx][k][5],
+                        u[idx][k][6],
+                        u[idx][k][7]
                     )
 
         # 装備
@@ -156,8 +170,6 @@ class App:
         for i in range(self.tower_num):
             T = len(self.tower_info[i])
             for j in range(T):
-                pyxel.rectb(40 + 100 * (i + 1) - slide,
-                            240 - 50 * j, 60, 50, 0)
                 t, b, m, idx = self.tower_info[i][j]
                 if t == 2:
                     pyxel.text(
@@ -166,36 +178,43 @@ class App:
                         f"{self.text[b - 1]}{m}",
                         0
                     )
-                    u = [
-                        [35, 24, 0, 0, 216, 16, 23, 5],
-                        [35, 24, 0, 24, 216, 16, 23, 5],
-                        [35, 24, 0, 48, 216, 16, 23, 5]
-                    ]
                     pyxel.blt(
-                        40 + 100 * (i + 1) - slide + u[idx][0],
-                        290 - 50 * j - u[idx][1],
-                        u[idx][2],
-                        u[idx][3],
-                        u[idx][4],
-                        u[idx][5],
-                        u[idx][6],
-                        u[idx][7]
+                        40 + 100 * (i + 1) - slide + v[idx][0],
+                        290 - 50 * j - v[idx][1],
+                        v[idx][2],
+                        v[idx][3],
+                        v[idx][4],
+                        v[idx][5],
+                        v[idx][6],
+                        v[idx][7]
                     )
 
     # ファイター
     def draw_fighter(self):
         slide = self.left_slide
 
+        u = (
+            (0, 32, 16, 32, 40, 5),
+            (0, 32, 56, 32, 40, 5),
+            (0, 64, 56, 40, 40, 5)
+        )
+
+        v = (
+            (0, 32, 16, 32, 40, 5),
+            (0, 32, 56, 32, 40, 5),
+            (0, 64, 56, 40, 40, 5)
+        )
+
+        w = (
+            (0, 0, 16, 32, 40, 5),
+            (0, 32, 16, 32, 40, 5)
+        )
+
         pyxel.load(self.load_path[0])
         # 戦っているときのアニメーション
         if self.is_fighting:
             # 倒すときのアニメーション
             if self.tower_info[self.fighter_now][self.on_fighting][0] == 1:
-                u = [
-                    [0, 32, 16, 32, 40, 5],
-                    [0, 32, 56, 32, 40, 5],
-                    [0, 64, 56, 40, 40, 5]
-                ]
                 i = (pyxel.frame_count - self.fighting_time) // 10 % len(u)
                 pyxel.blt(
                     142,
@@ -210,40 +229,31 @@ class App:
 
             # 装備などをとるときのアニメーション
             elif self.tower_info[self.fighter_now][self.on_fighting][0] == 2:
-                u = [
-                    [0, 32, 16, 32, 40, 5],
-                    [0, 32, 56, 32, 40, 5],
-                    [0, 64, 56, 40, 40, 5]
-                ]
-                i = (pyxel.frame_count - self.fighting_time) // 10 % len(u)
+                i = (pyxel.frame_count - self.fighting_time) // 10 % len(v)
                 pyxel.blt(
                     142,
                     250 - 50 * self.on_fighting,
-                    u[i][0],
-                    u[i][1],
-                    u[i][2],
-                    u[i][3],
-                    u[i][4],
-                    u[i][5]
+                    v[i][0],
+                    v[i][1],
+                    v[i][2],
+                    v[i][3],
+                    v[i][4],
+                    v[i][5]
                 )
 
         # 元の位置にいる
         else:
-            u = [
-                [0, 0, 16, 32, 40, 5],
-                [0, 32, 16, 32, 40, 5]
-            ]
             # self.thinkingでいい感じ
-            i = (pyxel.frame_count - self.thinking) // 20 % len(u)
+            i = (pyxel.frame_count - self.thinking) // 20 % len(w)
             pyxel.blt(
                 42 + self.fighter_now * 100 - slide,
                 250,
-                u[i][0],
-                u[i][1],
-                u[i][2],
-                u[i][3],
-                u[i][4],
-                u[i][5]
+                w[i][0],
+                w[i][1],
+                w[i][2],
+                w[i][3],
+                w[i][4],
+                w[i][5]
             )
 
     # メンバ変数のまとめ
@@ -259,19 +269,19 @@ class App:
         self.thinking = 0  # 自己満足です
 
         #
-        self.load_path = [
+        self.load_path = (
             "../assets/fighter.pyxres",
-            "../assets/enemy.pyxres"
-        ]
+            "../assets/enemy_neo.pyxres"
+        )
 
         #
-        self.problem_path = [
+        self.problem_path = (
             "../assets/choimuzu.txt",
             "../assets/test_easy.txt"
-        ]
+        )
 
         #
-        self.enemy_num = 6
+        self.enemy_num = 7
         self.equip_num = 3
 
         # 演算表示
